4b37ddd258a2acf66c0c561de08054eed7377eb5
alexey <alexeyfala@gmail.com>
2026-01-23 01:41:42 +0300

    Initial commit

---
+++ DataOrganizer.UnitTests/TestTypes/KeyboardInputHook.Tests.cs
@@ -0,0 +1,11 @@
+﻿using DataOrganizer.Services;
+
+namespace DataOrganizer.UnitTests.TestTypes;
+
+[TestFixture(Description = $@"Tests of ""{nameof(KeyboardInputHook)}"" type")]
+internal class KeyboardInputHookTests
+{
+	#region Methods
+
+	#endregion
+}
