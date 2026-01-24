0d662d23ca5432d35f4bbf0791b62b689a85bc87
alexey <alexeyfala@gmail.com>
2026-01-25 01:27:26 +0300

    Created EncryptionService

--- DataOrganizer/Interfaces/IEncryptionService.cs
+++ DataOrganizer/Interfaces/IEncryptionService.cs
@@ -2,4 +2,15 @@
 
 public interface IEncryptionService
 {
+	#region Methods
+	/// <summary>
+	/// Decrypts data.
+	/// </summary>
+	bool Decrypt(byte[] input, byte[] password, out byte[] output);
+
+	/// <summary>
+	/// Encrypts data.
+	/// </summary>
+	bool Encrypt(byte[] input, byte[] password, out byte[] output);
+	#endregion
 }
--- DataOrganizer/Services/EncryptionService.cs
+++ DataOrganizer/Services/EncryptionService.cs
@@ -1,8 +1,138 @@
 ﻿using DataOrganizer.Interfaces;
+using NSec.Cryptography;
+using Serilog;
+using Shared.Extensions;
+using System;
+using System.Security.Cryptography;
 
 namespace DataOrganizer.Services;
 
 public sealed class EncryptionService : IEncryptionService
 {
+	#region Data
+	/// <summary>
+	/// Salt size.
+	/// </summary>
+	private const int _saltSize = 16;
 
+	/// <summary>
+	/// The encryption algorithm used.
+	/// </summary>
+	private static readonly AeadAlgorithm _algorithm = AeadAlgorithm.XChaCha20Poly1305;
+
+	/// <inheritdoc cref="ILogger" />
+	private readonly ILogger _logger;
+	#endregion
+
+	#region Constructors
+	public EncryptionService(ILogger logger) => _logger = logger;
+	#endregion
+
+	#region Methods
+	/// <inheritdoc />
+	public bool Decrypt(byte[] input, byte[] password, out byte[] output)
+	{
+		output = [];
+
+		try
+		{
+			byte[] salt = new byte[_saltSize];
+
+			byte[] nonce = new byte[_algorithm.NonceSize];
+
+			byte[] cipherText = new byte[input.Length - salt.Length - nonce.Length];
+
+			Buffer.BlockCopy(input, 0, salt, 0, salt.Length);
+
+			Buffer.BlockCopy(input, salt.Length, nonce, 0, nonce.Length);
+
+			Buffer.BlockCopy(input, salt.Length + nonce.Length, cipherText, 0, cipherText.Length);
+
+			using Key key = DeriveKey(password, salt);
+
+			if (_algorithm.Decrypt(
+				key: key,
+				nonce: nonce,
+				associatedData: [],
+				ciphertext: cipherText) is { } result)
+			{
+				output = result;
+
+				return true;
+			}
+
+			return false;
+		}
+		catch (Exception ex)
+		{
+			_logger.LogException(ex);
+
+			return false;
+		}
+	}
+
+	/// <inheritdoc />
+	public bool Encrypt(byte[] input, byte[] password, out byte[] output)
+	{
+		output = [];
+
+		try
+		{
+			byte[] salt = RandomNumberGenerator.GetBytes(_saltSize);
+
+			byte[] nonce = RandomNumberGenerator.GetBytes(_algorithm.NonceSize);
+
+			using Key key = DeriveKey(password, salt);
+
+			byte[] cipherText = _algorithm.Encrypt(
+				key,
+				nonce,
+				associatedData: [],
+				input);
+
+			byte[] result = new byte[salt.Length + nonce.Length + cipherText.Length];
+
+			Buffer.BlockCopy(salt, 0, result, 0, salt.Length);
+
+			Buffer.BlockCopy(nonce, 0, result, salt.Length, nonce.Length);
+
+			Buffer.BlockCopy(cipherText, 0, result, salt.Length + nonce.Length, cipherText.Length);
+
+			output = result;
+
+			return true;
+		}
+		catch (Exception ex)
+		{
+			_logger.LogException(ex);
+
+			return false;
+		}
+	}
+	#endregion
+
+	#region Service
+	/// <summary>
+	/// Derives a key.
+	/// </summary>
+	private static Key DeriveKey(byte[] password, byte[] salt)
+	{
+		Argon2id kdf = PasswordBasedKeyDerivationAlgorithm.Argon2id(new()
+		{
+			MemorySize = 131072, // 128 MB
+			NumberOfPasses = 3,
+			DegreeOfParallelism = 1
+		});
+
+		byte[] keyMaterial = kdf.DeriveBytes(
+			password,
+			salt,
+			_algorithm.KeySize);
+
+		return Key.Import(
+			_algorithm,
+			keyMaterial,
+			KeyBlobFormat.RawSymmetricKey);
+	}
+	#endregion
 }
