28ab967d7a76213526cea6cdbf1a4a4847a823a8
alexey <alexeyfala@gmail.com>
2026-01-28 02:22:09 +0300

    EncryptionViewModel

--- DataOrganizer/ViewModels/EncryptionViewModel.cs
+++ DataOrganizer/ViewModels/EncryptionViewModel.cs
@@ -1,11 +1,27 @@
-﻿using DataOrganizer.Views;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using DataOrganizer.Views;
 
 namespace DataOrganizer.ViewModels;
 
 /// <summary>
 /// View model for <see cref="EncryptionView" />.
 /// </summary>
-public sealed class EncryptionViewModel
+public sealed partial class EncryptionViewModel : ObservableObject
 {
+	#region Auto-Generated Properties
+	/// <summary>
+	/// Error related to <see cref="MasterPasswordFilePath" />.
+	/// </summary>
+	[ObservableProperty]
+	private string? _masterPasswordFileError;
 
+	/// <summary>
+	/// Information related to <see cref="MasterPasswordFilePath" />.
+	/// </summary>
+	[ObservableProperty]
+	private string? _masterPasswordFileInfo;
+
+	[ObservableProperty]
+	private string? _masterPasswordFilePath;
+	#endregion
 }
