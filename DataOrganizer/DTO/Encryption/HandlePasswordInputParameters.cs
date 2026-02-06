340409973fe58b2a42fbd7af78f06e1510cc6fcb
alexey <alexeyfala@gmail.com>
2026-02-06 16:33:39 +0300

    HandlePasswordInputParameters added

---
+++ DataOrganizer/DTO/Encryption/HandlePasswordInputParameters.cs
@@ -0,0 +1,22 @@
+﻿using DataOrganizer.DTO.Entities.Models;
+using DataOrganizer.Views;
+
+namespace DataOrganizer.DTO.Encryption;
+
+/// <summary>
+/// The parameters for password input for encryption/decryption files in folder.
+/// </summary>
+public class HandlePasswordInputParameters : TakeCryptPasswordParameters
+{
+	#region Properties
+	/// <summary>
+	/// A sequence to <see cref="FileModelDto" /> objects.
+	/// </summary>
+	public required FileModelDto[] Files { get; init; }
+
+	/// <summary>
+	/// A reference to <see cref="PasswordBox" />.
+	/// </summary>
+	public required PasswordBox View { get; init; }
+	#endregion
+}
