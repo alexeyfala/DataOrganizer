9385065555e4c4cee46e327da76b2a73be71a329
alexey <alexeyfala@gmail.com>
2026-02-06 16:21:57 +0300

    TakeCryptPasswordParameters added

---
+++ DataOrganizer/DTO/Encryption/TakeCryptPasswordParameters.cs
@@ -0,0 +1,18 @@
+﻿using DataOrganizer.DTO.Entities.Models;
+using DataOrganizer.Enums;
+
+namespace DataOrganizer.DTO.Encryption;
+
+/// <summary>
+/// The parameters for encryption/decryption files in folder.
+/// </summary>
+public class TakeCryptPasswordParameters
+{
+	#region Properties
+	/// <inheritdoc cref="CryptoAction" />
+	public required CryptoAction Action { get; init; }
+
+	/// <inheritdoc cref="FolderModelDto" />
+	public required FolderModelDto Folder {  get; init; }
+	#endregion
+}
