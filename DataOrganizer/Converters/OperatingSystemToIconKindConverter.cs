33c7e88b194075eb3494f50668ea338255150a78
alexey <alexeyfala@gmail.com>
2026-02-21 21:43:21 +0300

    OperatingSystemToIconKindConverter added

---
+++ DataOrganizer/Converters/OperatingSystemToIconKindConverter.cs
@@ -0,0 +1,47 @@
+﻿using Avalonia.Data.Converters;
+using Material.Icons;
+using Shared.Common;
+using System;
+using System.Globalization;
+
+namespace DataOrganizer.Converters;
+
+internal sealed class OperatingSystemToIconKindConverter : IValueConverter
+{
+	#region Methods
+	/// <inheritdoc />
+	public object? Convert(
+		object? value,
+		Type targetType,
+		object? parameter,
+		CultureInfo culture)
+	{
+		if (AppUtils.IsWindows)
+		{
+			return MaterialIconKind.MicrosoftWindows;
+		}
+
+		if (AppUtils.IsLinux)
+		{
+			return MaterialIconKind.Linux;
+		}
+
+		if (AppUtils.IsMacOs)
+		{
+			return MaterialIconKind.Apple;
+		}
+
+		return default(MaterialIconKind);
+	}
+
+	/// <inheritdoc />
+	public object? ConvertBack(
+		object? value,
+		Type targetType,
+		object? parameter,
+		CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+	#endregion
+}
