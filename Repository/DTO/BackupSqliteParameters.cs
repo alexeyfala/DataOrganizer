544f37de1e150354fbb8b0e3cf068f426b0da072
alexey <alexeyfala@gmail.com>
2026-03-05 10:49:52 +0300

    BackupSqliteParameters added

---
+++ Repository/DTO/BackupSqliteParameters.cs
@@ -0,0 +1,29 @@
+﻿namespace Repository.DTO;
+
+/// <summary>
+/// Parameters for SQlite database backup.
+/// </summary>
+public readonly struct BackupSqliteParameters
+{
+	#region Properties
+	/// <summary>
+	/// Returns <c>True</c> if the connection pool for the database at <see cref="DestFilePath" /> should be cleared.
+	/// </summary>
+	public required bool ClearDestPool { get; init; }
+
+	/// <summary>
+	/// Returns <c>True</c> if the connection pool for the database at <see cref="SourceFilePath" /> should be cleared.
+	/// </summary>
+	public required bool ClearSourcePool { get; init; }
+
+	/// <summary>
+	/// Absolute path to the destination file.
+	/// </summary>
+	public required string DestFilePath { get; init; }
+
+	/// <summary>
+	/// Absolute path to the source file.
+	/// </summary>
+	public required string SourceFilePath { get; init; }
+	#endregion
+}
