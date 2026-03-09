b892526c3b268210f9e00d3d3dc0a306f6d48d8d
alexey <alexeyfala@gmail.com>
2026-03-09 17:44:08 +0300

    Initial commit

---
+++ DataOrganizer.UnitTests/TestTypes/DataExchangeService.Tests.cs
@@ -0,0 +1,11 @@
+﻿using DataOrganizer.Services;
+
+namespace DataOrganizer.UnitTests.TestTypes;
+
+[TestFixture(Description = $@"Tests of ""{nameof(DataExchangeService)}"" type")]
+internal class DataExchangeServiceTests
+{
+	#region Methods
+
+	#endregion
+}
