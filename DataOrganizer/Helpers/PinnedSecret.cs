2f35b794a65bd41b4c4571eb1a6d930ef834cdd8
alexey <alexeyfala@gmail.com>
2026-04-01 23:54:38 +0300

    SecureStringHelper added

---
+++ DataOrganizer/Helpers/PinnedSecret.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+namespace DataOrganizer.Helpers;
+
+internal sealed class PinnedSecret : IDisposable
+{
+	#region Properties
+	public int Length => _buffer.Length;
+	#endregion
+
+	#region Data
+	private readonly char[] _buffer;
+
+	private readonly GCHandle _handle;
+
+	private bool _isDisposed;
+	#endregion
+
+	#region Constructors
+	public PinnedSecret(int length)
+	{
+		_buffer = new char[length];
+
+		_handle = GCHandle.Alloc(_buffer, GCHandleType.Pinned); // Don't let GC copy
+	}
+	#endregion
+
+	#region Methods
+	public ReadOnlySpan<char> AsReadOnlySpan() => _buffer.AsSpan();
+
+	public Span<char> AsSpan() => _buffer.AsSpan();
+
+	/// <inheritdoc />
+	public void Dispose()
+	{
+		if (_isDisposed)
+		{
+			return;
+		}
+
+		_isDisposed = true;
+
+		CryptographicOperations.ZeroMemory(MemoryMarshal.AsBytes(_buffer.AsSpan()));
+
+		_handle.Free();
+	}
+	#endregion
+}
---
+++ DataOrganizer/Helpers/SecureStringHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DataOrganizer.Helpers;
+
+internal static class SecureStringHelper
+{
+	#region Methods
+	/// <summary>
+	/// Copies a string into the pinned array and attempts to zero out the original.
+	/// </summary>
+	public static PinnedSecret CaptureAndWipe(string source)
+	{
+		PinnedSecret secret = new(source.Length);
+
+		source
+			.AsSpan()
+			.CopyTo(secret.AsSpan());
+
+		WipeString(source);
+
+		return secret;
+	}
+	#endregion
+
+	#region Service
+	/// <summary>
+	/// Tries to wipe a string in memory.
+	/// </summary>
+	private static void WipeString(string value)
+	{
+		Span<char> span = MemoryMarshal.CreateSpan(
+			ref MemoryMarshal.GetReference(value.AsSpan()),
+			value.Length);
+
+		MemoryMarshal
+			.AsBytes(span)
+			.Clear();
+	}
+	#endregion
+}
