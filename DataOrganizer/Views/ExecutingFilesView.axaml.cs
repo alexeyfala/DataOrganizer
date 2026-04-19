ab176d62264fbd733de02cb258cb3b82aa0b359e
alexey <alexeyfala@gmail.com>
2026-04-19 17:30:55 +0300

    ExecutingFilesView

---
+++ DataOrganizer/Views/ExecutingFilesView.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls;
+
+namespace DataOrganizer.Views;
+
+internal sealed partial class ExecutingFilesView : UserControl
+{
+	#region Constructors
+	public ExecutingFilesView() => InitializeComponent();
+	#endregion
+}
