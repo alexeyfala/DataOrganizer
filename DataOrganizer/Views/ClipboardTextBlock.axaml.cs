3cfda836d1675ec980d7363d7d395d51ff8cd0ac
alexey <alexeyfala@gmail.com>
2026-05-17 00:12:25 +0300

    Initial commit

--- DataOrganizer/Views/ClipboardTextBlock.axaml.cs
+++ DataOrganizer/Views/ClipboardTextBlock.axaml.cs
@@ -252,6 +252,7 @@ internal sealed partial class ClipboardTextBlock : UserControl
 	[RelayCommand(CanExecute = nameof(CanCopySelectedNote))]
 	private void CopySelectedNote(SelectableTextBlock? target) => target?.Copy();
 
+
 	/// <summary>
 	/// Copies <see cref="Text" /> value to system clipboard.
 	/// </summary>
