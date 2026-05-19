abecd82598cbd083e5a2049888de552dc710eff8
alexey <alexeyfala@gmail.com>
2026-05-20 01:36:25 +0300

    ImportDataResult added

---
+++ DataOrganizer/DTO/ImportDataResult.cs
@@ -0,0 +1,18 @@
+﻿using DataOrganizer.DTO.Entities.Abstract;
+using DataOrganizer.Enums;
+using System.Collections.Generic;
+
+namespace DataOrganizer.DTO;
+
+public sealed class ImportDataResult
+{
+	#region Properties
+	/// <summary>
+	/// Imported items.
+	/// </summary>
+	public required IEnumerable<ExplorerModelBaseDto> ImportedItems { get; init; }
+
+	/// <inheritdoc cref="ImportListVariant" />
+	public required ImportListVariant Variant { get; init; }
+	#endregion
+}
--- DataOrganizer/Enums/ImportListVariant.cs
+++ DataOrganizer/Enums/ImportListVariant.cs
@@ -1,5 +1,8 @@
 ﻿namespace DataOrganizer.Enums;
 
+/// <summary>
+/// Variant of populating objects in list when importing.
+/// </summary>
 public enum ImportListVariant
 {
 	None,
