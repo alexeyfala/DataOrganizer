a14e5f63dd48c6e159394447de29bb7fe2e459c1
alexey <alexeyfala@gmail.com>
2026-05-25 01:24:06 +0300

    FileAssociationService

--- DataOrganizer/Services/ExecutionEngine.cs
+++ DataOrganizer/Services/ExecutionEngine.cs
@@ -258,6 +258,11 @@ public sealed class ExecutionEngine : IExecutionEngine
 
 				string filePath = Path.Combine(directoryPath, fileName);
 
+				var x = _fileAssociation.GetApplicationByExtension(Path.GetExtension(fileName));
+
+				var y = _fileAssociation.GetApplicationByPath(filePath);
+
+
 				string? selectedAppPath = null;
 
 				if (AppUtils.IsWindows)
