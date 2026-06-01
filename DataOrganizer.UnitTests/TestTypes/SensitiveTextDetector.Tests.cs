70145cb4ed74f2a46cd7383fb216766e01306d44
alexey <alexeyfala@gmail.com>
2026-06-01 23:28:47 +0300

    SensitiveTextDetectorTests added

---
+++ DataOrganizer.UnitTests/TestTypes/SensitiveTextDetector.Tests.cs
@@ -0,0 +1,11 @@
+﻿using DataOrganizer.Helpers;
+
+namespace DataOrganizer.UnitTests.TestTypes;
+
+[TestFixture(Description = $@"Tests of ""{nameof(SensitiveTextDetector)}"" type")]
+internal class SensitiveTextDetectorTests
+{
+	#region Methods
+
+	#endregion
+}
