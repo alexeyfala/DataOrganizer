12538a92ff059cae3007a67967a4d285fee499ed
alexey <alexeyfala@gmail.com>
2026-06-07 16:14:10 +0300

    Initial commit

--- DataOrganizer.UnitTests/TestTypes/ClipboardHistoryService.Tests.cs
+++ DataOrganizer.UnitTests/TestTypes/ClipboardHistoryService.Tests.cs
@@ -18,7 +18,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-
 namespace DataOrganizer.UnitTests.TestTypes;
 
 [TestFixture(Description = $@"Tests of ""{nameof(ClipboardHistoryService)}"" type")]
