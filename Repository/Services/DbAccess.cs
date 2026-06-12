4e4129f0fd874a8862e83a34b96bc4594a09820a
alexey <alexeyfala@gmail.com>
2026-06-13 01:16:29 +0300

    Initial commit

--- Repository/Services/DbAccess.cs
+++ Repository/Services/DbAccess.cs
@@ -377,6 +377,7 @@ public sealed class DbAccess : IDbAccess
 				.WaitAsync(token)
 				.ConfigureAwait(false);
 
+
 			_logger.LogInformation("Connecting to the database.");
 
 			await (_dbContextService.HasMigrations(Assembly.GetExecutingAssembly())
