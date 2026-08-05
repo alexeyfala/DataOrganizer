274ca60312b3ca72ffb964693eac13bbe3517121
alexey <alexeyfala@gmail.com>
2026-08-06 00:31:48 +0300

    NoteView.axaml added

---
+++ DataOrganizer/Interfaces/INoteReader.cs
@@ -0,0 +1,14 @@
+namespace DataOrganizer.Interfaces;
+
+/// <summary>
+/// Provides the note of an object as plain text.
+/// </summary>
+public interface INoteReader
+{
+	#region Methods
+	/// <summary>
+	/// Returns the note as plain text, or <c>null</c> when it is unavailable.
+	/// </summary>
+	string? ReadNote(object? item);
+	#endregion
+}
